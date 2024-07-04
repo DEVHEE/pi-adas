f422b36d589f09ed7836b4df39ad82482b00d640
KIM DONGHEE <hee.sincostan@gmail.com>
2024-07-05 03:34:37 +0900

    Create pca9685_servo.py

---
+++ pca9685_servo.py
@@ -0,0 +1,60 @@
+import time
+import board
+
+# pip install adafruit-circuitpython-pca9685
+from adafruit_motor import servo
+from adafruit_pca9685 import PCA9685
+
+
+def initHandle():
+    for i in range(45):
+        servo_handle.angle = 90 - i
+        time.sleep(0.005)
+    for i in range(90):
+        servo_handle.angle = 45 + i
+        time.sleep(0.005)
+    for i in range(90):
+        servo_handle.angle = 135 - i
+        time.sleep(0.002)
+    for i in range(90):
+        servo_handle.angle = 45 + i
+        time.sleep(0.002)
+    time.sleep(0.3)
+    servo_handle.angle = 90
+
+
+def initCameraLR():
+    for i in range(45):
+        servo_cameraLR.angle = 80 + i
+        time.sleep(0.005)
+    print(servo_cameraLR.angle)
+    for i in range(90):
+        servo_cameraLR.angle = 125 - i
+        time.sleep(0.005)
+    for i in range(45):
+        servo_cameraLR.angle = 35 + i
+        time.sleep(0.005)
+        
+        
+def initCameraUD():
+    for i in range(45):
+        servo_cameraUD.angle = 60 + i
+        time.sleep(0.005)
+    time.sleep(0.5)
+    for i in range(45):
+        servo_cameraUD.angle = 105 - i
+        time.sleep(0.005)   
+
+
+i2c = board.I2C()
+
+pca = PCA9685(i2c)
+pca.frequency = 50
+
+servo_handle = servo.Servo(pca.channels[0])
+servo_cameraLR = servo.Servo(pca.channels[1])
+servo_cameraUD = servo.Servo(pca.channels[2])
+
+initHandle()  # LEFT 45 - 90 - 135 RIGHT
+initCameraLR()  # LEFT 125 - 80 - 35 RIGHT
+initCameraUD()  # DOWN 60 - 105 UP
